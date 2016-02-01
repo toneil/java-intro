ddb4c233bca8993440dc58288fa0b8cd8ff420bb
Tobias Neil <brortobiasneil@gmail.com>
2016-02-01 11:07:50 +0100

    things

---
+++ reference/basics/BasicDataStructures.java
@@ -0,0 +1,9 @@
+import java.lang.String;
+import java.util.ArrayList;
+
+public class BasicDataStructures {
+
+    public static void main(String[] argv) {
+
+    }
+}
--- reference/basics/README.md
+++ reference/basics/README.md
@@ -5,3 +5,4 @@ Take a look at these classes in the following order:
 1. HelloWorld.java
 2. TypesAndVariabels.java
 3. ControlFlow.java
+4. BasicDataStructures.java
