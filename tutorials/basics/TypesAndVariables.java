dad3435bf9c87575807bfe702c927f209c1a6f7f
Tobias Neil <brortobiasneil@gmail.com>
2016-02-02 16:55:40 +0100

    New stuff

--- tutorials/basics/TypesAndVariables.java
+++ tutorials/basics/TypesAndVariables.java
@@ -7,27 +7,27 @@ public class TypesAndVariables {
 		// Once it's declared we can manipulate it in the familiar Python syntax. 
 		// However, variables can't change type!
 
-		int a = 1
+		int a = 1;
 		a = 4+2;
 		// a = "Some string"; <- this will cause the compilation to fail
 
 		// There are a couple of "primitive" (i.e. not object-based) types in Java
 		int x = 3;
-		float f = 3.14159;
+		float f = 3.14f;
 		char c = 'C';
 
 
 		// int can hold numbers between -2147483648 and 2147483647 and float can hold a 32 bit decimal number. 
 		// For larger values (or more precise decimals) we can use long and double
 
-		long y = 53867928414636532;
+		long y = 5386792;
 		double d = 2.000000000000000002421452;
 
 		// String is not a primitive type, but a class. 
 		// Strings have methods attached to them, as opposed to ints and chars. 
 
 		String str = "Hello!";
-		System.out.println(str.toUpper());
+		System.out.println(str.toUpperCase());
 
 		// Take a look at the methods below
 
